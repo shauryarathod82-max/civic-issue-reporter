d4a25d130de76862a33d6aaf967dbc523957f23c
shauryaa rathod <shauryarathod82@gmail.com>
2026-03-28 14:56:05 +0530

    Create Issue.java

---
+++ Issue.java
@@ -0,0 +1,16 @@
+public class Issue {
+    String title;
+    String location;
+    boolean resolved;
+
+    public Issue(String title, String location) {
+        this.title = title;
+        this.location = location;
+        this.resolved = false;
+    }
+
+    public void display() {
+        System.out.println("Issue: " + title + " | Location: " + location + 
+        " | Status: " + (resolved ? "Resolved" : "Pending"));
+    }
+}
